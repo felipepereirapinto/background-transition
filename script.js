eb6c2abd5211cc0881c255bd5b59912d1e5e2745
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2021-06-11 17:32:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const images = [
+  'canada-0.jpg',
+  'canada-1.jpg',
+  'canada-2.jpg',
+  'canada-3.jpg',
+]
+const base = 'assets/'
+
+const style = document.documentElement.style
+let index = 1;
+
+// Changes background every 4 seconds
+setInterval(() => {
+  style.background = `url('${base}${images[index]}') no-repeat center center fixed`
+  style.backgroundSize = 'cover'
+
+  index >= images.length - 1 ? index = 0 : index++
+}, 4000);
